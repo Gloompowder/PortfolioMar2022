368cf172521709f58ffdac9751f516bcfe8d9064
William <willlin1996@gmail.com>
2022-03-04 09:53:26 -0500

    save

--- portfoliomar2022/src/CodingCard.js
+++ portfoliomar2022/src/CodingCard.js
@@ -1,11 +1,21 @@
 import * as React from "react";
 
 function CodingCard(props) {
-    console.log(props)
+    const key = Object.keys(props.project);
+    const description = Object.values(props.project)[0].description
+    const frontend = Object.values(props.project)[0].frontend
+    const backend = Object.values(props.project)[0].backend
+
+    console.log(backend)
     return (
         <div className={props.id}>
             <h1>
+                {Object.keys(props.project)}
             </h1>
+            <p>
+                {description}
+            </p>
+
         </div>
     );
   }
