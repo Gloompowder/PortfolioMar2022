782ab4e52568ca3e1084ae75af4459340234626f
William <willlin1996@gmail.com>
2022-03-07 01:01:53 -0500

    save

--- portfoliomar2022/src/Modal.js
+++ portfoliomar2022/src/Modal.js
@@ -1,4 +1,6 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
+import { BiCode, BiPalette, BiBrain, BiMenu, BiPhone } from "react-icons/bi";
 
 function Modal(props) {
     const modal = props.modal;
@@ -6,7 +8,15 @@ function Modal(props) {
     <div id="myModal" className="modal" >
     <div className="modal-content">
     <span className="close" onClick={()=>props.spanToggle}>&times;</span>
-    <p>Some text in the Modal..</p>
+    <Link className= "modal-home" to="/">William Lin</Link>
+        <ul className = "modal-quad">
+            <ul className = "modal-trio">
+            <Link className = "modal-about-me" to="/about-me" > <BiBrain /> About Me</Link>
+            <Link className = "modal-coding" to="/coding" > <BiCode /> Coding</Link>
+            <Link className = "modal-design" to="/design" > <BiPalette /> Design</Link>
+            </ul>
+            <Link className ="modal-contact" to="/contact" > <BiPhone />Contact</Link>
+        </ul>
     </div>
 </div>
     );
